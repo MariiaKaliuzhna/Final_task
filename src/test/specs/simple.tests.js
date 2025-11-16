fb77cffa427264b80d02889c984d383cc6d8ae75
Mariia Kaliuzhna <comfy51463@gmail.com>
2025-11-16 06:35:02 +0200

    Final version

--- src/test/specs/simple.tests.js
+++ src/test/specs/simple.tests.js
@@ -1,16 +1,15 @@
 const { pages } = require("./../../po");
 const fs = require("fs");
-const path = require("path");
-
 const testData = JSON.parse(fs.readFileSync("src/test/data/testData.json", "utf-8"));
 
-describe ("Login", async() => {
+describe ("Login", () => {
     beforeEach (async() => {
         await browser.url("/");
     });
 
     testData.forEach(data => {
         it(`${data.id} : ${data.name} `, async() => {
+            console.log(`Input for username: ${data.username} and for password: ${data.password} - (${data.id})`);
             const loginInput = pages("login").loginInput;
             await Promise.all([
                 loginInput.input("username").setValue(data.username),
@@ -21,17 +20,24 @@ describe ("Login", async() => {
                 if (data.expectedResult === "Epic sadface: Username is required"){
                     await loginInput.clearField(loginInput.input("username"));
                     await loginInput.clearField(loginInput.input("password"));
-                } else 
+                    console.log("Cleared both input fields");
+                } else {
                     await loginInput.clearField(loginInput.input("password"));
+                    console.log("Cleared only password input field")
+                }
             }
 
             await pages("login").loginInput.loginBtn.click();
+            console.log("Clicked login button")
 
             if (!data.isPositiveTesting){
                 await pages("login").errorMessage.errorText.waitForDisplayed();
                 await expect(pages("login").errorMessage.errorText).toHaveText(data.expectedResult);
-            } else 
+                console.log(`Logging failed with message: ${data.expectedResult}`);
+            } else {
                 await expect(pages("catalogue").catalogueTitle.textTitle).toHaveText("Swag Labs");
+                console.log("Successfully logged in");
+            }
         });
     });
 });
